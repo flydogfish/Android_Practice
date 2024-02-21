efb3d54d874bf6b2d9fa445792b735d91df4fef2
flydogfish <1784691138@qq.com>
2024-02-22 00:08:58 +0800

    2024-02-22学习提交

--- app/src/main/java/com/example/chapter6/MainActivity.java
+++ app/src/main/java/com/example/chapter6/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.chapter6;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TimeworkChangeReceiver timeworkChangeReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 设置过滤器用来监听每分钟系统时间的变化
+        // 定义一个过滤器和网络广播
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.intent.action.TIME_TICK");
+
+        timeworkChangeReceiver = new TimeworkChangeReceiver();
+        registerReceiver(timeworkChangeReceiver, intentFilter);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 当activity的销毁周期会注销广播注册
+        unregisterReceiver(timeworkChangeReceiver);
+    }
+
+    static class TimeworkChangeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "time changes", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
